60bc45e77ef87bcd84fd219df89c1216fb6c7041
07almasashimbekov <75465336+07almasashimbekov@users.noreply.github.com>
2024-09-04 08:58:11 +0600

    first push

---
+++ test.py
@@ -0,0 +1,27 @@
+a = int(input('a = '))
+
+b = int(input('b = '))
+
+c = int(input('c = '))
+
+d = int(input('d = '))
+
+summ = 0
+
+if a % 3 == 0:
+
+   summ = summ + a
+
+if b % 3 == 0:
+
+   summ = summ + b
+
+if c % 3 == 0:
+
+   summ = summ + c
+
+if d % 3 == 0:
+
+   summ = summ + d
+
+print(summ)
